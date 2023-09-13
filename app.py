b6382caea88dc209b8e2ca15738bcb446c00fcab
Смакотин Степан <stepansmakotin@ya.ru>
2023-09-13 17:35:23 +0700

    Статические фафлы

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect
+from flask import Flask, redirect, url_for
 app = Flask(__name__)
 
 @app.route("/")
@@ -53,3 +53,16 @@ def lab():
     </body>
 </html>
 """
+@app.route("/lab1/oak")
+def oak ():
+    return '''
+<!doctype html>
+<html>
+<link rel="stylesheet" href="''' + url_for ('static', filename='lab1.css') + '''">
+    <body>
+        <h1>Дуб</h1>
+        <img src="''' + url_for ('static', filename='maxresdefault.jpg') + '''">
+    </body>
+</html>
+
+'''
