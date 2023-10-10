79f759f7bc6bc8a9ff4572f1d49a655997fcfe4c
Смакотин Степан <stepansmakotin@ya.ru>
2023-10-11 02:43:49 +0700

    Книги

--- app.py
+++ app.py
@@ -123,7 +123,7 @@ def bmw ():
 
 @app.route('/lab2/example')
 def example ():
-    name, number, group, course = 'Егор Панченко', 2, 'ФБИ-13', '3 курс'
+    name, number, group, course = 'Степан Смакотин', 2, 'ФБИ-13', '3 курс'
     fruits = [
         {'name': 'яблоки', 'price': 100},
         {'name': 'груши', 'price': 120},
@@ -131,4 +131,15 @@ def example ():
         {'name': 'мандарины', 'price': 95}, 
         {'name': 'манго', 'price': 321},
     ]
-    return render_template('example.html', name=name, number=number, group=group, course=course, fruits=fruits)
+    books = [
+        { 'author': 'Антуан де Сент-Экзюпери', 'name': 'Маленький приц', 'genre': 'повесть', 'pages': '160'},
+        { 'author': 'Джером Д. Сэлинджер', 'name': 'Над пропастью во ржи', 'genre': 'роман', 'pages': '320'},
+        { 'author': 'Теодор Драйзер', 'name': 'Финансит', 'genre': 'роман', 'pages': '736'},
+        { 'author': 'Пауло Коэльо', 'name': 'Алхимик', 'genre': 'роман', 'pages': '224'},
+        { 'author': 'Оскар Уальд', 'name': 'Портрет Дориана Грея', 'genre': 'роман', 'pages': '320'},
+        { 'author': 'Робин Шарма', 'name': 'Монах, который продал свой Феррари', 'genre': 'притча', 'pages': '256'},
+        { 'author': 'Питер Камп', 'name': 'Скорочтение: Как запоминать больше, читая в 8 раз быстрее', 'genre': 'саморазвитие', 'pages': '320'},
+        { 'author': 'Дуглас Адамс', 'name': 'Автостопом по галактике', 'genre': 'роман', 'pages': '256'},
+        { 'author': 'Фрэнсис Скотт Фицджеральд', 'name': 'Великий Гэтсби', 'genre': 'роман', 'pages': '256'},
+    ]
+    return render_template('example.html', name=name, number=number, group=group, course=course, fruits=fruits, books=books)
