124d48f1a5dfc437a880b91ccf18c2bcb3108e62
Смакотин Степан <stepansmakotin@ya.ru>
2023-11-22 11:49:44 +0700

    Авторизация

--- lab4.py
+++ lab4.py
@@ -5,3 +5,23 @@ lab4 = Blueprint('lab4', __name__)
 @lab4.route('/lab4/')
 def lab():
     return render_template('lab4.html')
+
+@lab4.route('/lab4/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    username = request.form.get('username')
+    password = request.form.get('password')
+    if not username:
+        error = 'Не введен логин'
+        return render_template ('login.html', error=error, username=username, password=password)
+    
+    if not password:
+        error = 'Не введен пароль'
+        return render_template ('login.html', error=error, username=username, password=password)
+    
+    if username == 'stepan' and password == '5252':
+        return render_template('success1.html', username=username)
+    
+    error = 'Неверные логин и/или пароль'
+    return render_template('login.html', error=error, username=username, password=password)
